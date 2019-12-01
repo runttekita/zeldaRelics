1f3943d976f26028ea585cdac3325c04601cb0d2
velvet-halation <buizark@gmail.com>
2019-11-30 17:57:25 -0800

    modifier stuff

--- src/zeldaRelics/modifiers/Hylian.kt
+++ src/zeldaRelics/modifiers/Hylian.kt
@@ -1,9 +1,14 @@
 package zeldaRelics.modifiers
 
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.daily.mods.AbstractDailyMod
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.helpers.RelicLibrary
+import com.megacrit.cardcrawl.relics.AbstractRelic
 import zeldaRelics.ZeldaRelics.Companion.makeID
 import zeldaRelics.ZeldaRelics.Companion.textureLoader
+import zeldaRelics.relics.AbstractZeldaRelic
 
 
 class Hylian : AbstractDailyMod(id, name, desc, null, true) {
@@ -17,6 +22,37 @@ class Hylian : AbstractDailyMod(id, name, desc, null, true) {
 
     init {
         img = textureLoader.getTexture("zeldaRelicsResources/images/ui/hylian.png")
+    }
+
+    @SpirePatch(
+        clz = AbstractDungeon::class,
+        method = "initializeRelicList"
+    )
+    object RemoveRelicsIfNotHylian {
+        @JvmStatic
+        fun Postfix(__instance: AbstractDungeon) {
+            if (CardCrawlGame.trial == null || !CardCrawlGame.trial.dailyModIDs().contains(Hylian.id)) {
+                removeRelics(AbstractDungeon.commonRelicPool)
+                removeRelics(AbstractDungeon.uncommonRelicPool)
+                removeRelics(AbstractDungeon.rareRelicPool)
+                removeRelics(AbstractDungeon.bossRelicPool)
+                removeRelics(AbstractDungeon.shopRelicPool)
+            }
+            println(AbstractDungeon.commonRelicPool)
+        }
+
+        fun removeRelics(list: ArrayList<String>) {
+            val relicsToRemove = ArrayList<String>()
+            for (relic in list) {
+                if (RelicLibrary.getRelic(relic) is AbstractZeldaRelic) {
+                    relicsToRemove.add(relic)
+                }
+            }
+            for (relic in relicsToRemove) {
+                AbstractDungeon.commonRelicPool.remove(relic)
+            }
+            relicsToRemove.clear()
+        }
     }
 
 }

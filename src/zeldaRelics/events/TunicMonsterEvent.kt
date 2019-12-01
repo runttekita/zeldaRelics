b52ec4091f838595efb8f40ff22440156f8f3309
velvet-halation <buizark@gmail.com>
2019-11-30 18:26:01 -0800

    i have no idea if any of this works

--- src/zeldaRelics/events/TunicMonsterEvent.kt
+++ src/zeldaRelics/events/TunicMonsterEvent.kt
@@ -1,17 +1,26 @@
 package zeldaRelics.events
 
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.actions.unique.IncreaseMaxHpAction
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.events.AbstractImageEvent
+import com.megacrit.cardcrawl.events.exordium.Mushrooms
 import com.megacrit.cardcrawl.helpers.MonsterHelper
 import com.megacrit.cardcrawl.helpers.PotionHelper
 import com.megacrit.cardcrawl.helpers.RelicLibrary
 import com.megacrit.cardcrawl.potions.FairyPotion
+import com.megacrit.cardcrawl.powers.MetallicizePower
 import com.megacrit.cardcrawl.rewards.RewardItem
+import javassist.CannotCompileException
+import javassist.NotFoundException
+import javassist.expr.ExprEditor
+import javassist.expr.Instanceof
 import zeldaRelics.ZeldaRelics.Companion.makeID
 import zeldaRelics.relics.BlueTunic
 import zeldaRelics.relics.RedTunic
 import zeldaRelics.rewards.LinkedRewardItem
+
 
 class TunicMonsterEvent : AbstractImageEvent(name, desc[0], "zeldaRelicResources/images/events/tunicMonster.png") {
 
@@ -21,6 +30,8 @@ class TunicMonsterEvent : AbstractImageEvent(name, desc[0], "zeldaRelicResources
         private val name = eventStrings.NAME
         private val desc = eventStrings.DESCRIPTIONS
         private val options = eventStrings.OPTIONS
+        private const val armorAmt = 3
+        private const val hpIncrease = 1.2f
     }
 
     init  {
@@ -48,6 +59,10 @@ class TunicMonsterEvent : AbstractImageEvent(name, desc[0], "zeldaRelicResources
             }
             1 -> {
                 AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter(AbstractDungeon.eliteMonsterList[0])
+                for (monster in AbstractDungeon.getMonsters().monsters) {
+                    AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(monster, monster, MetallicizePower(monster, armorAmt), armorAmt))
+                    AbstractDungeon.actionManager.addToBottom(IncreaseMaxHpAction(monster, hpIncrease, true))
+                }
                 enterCombatFromImage()
                 val blueTunicReward = LinkedRewardItem(RewardItem(RelicLibrary.getRelic(BlueTunic.id).makeCopy()))
                 val linkedTunics = LinkedRewardItem(blueTunicReward, RelicLibrary.getRelic(RedTunic.id).makeCopy());
@@ -59,5 +74,23 @@ class TunicMonsterEvent : AbstractImageEvent(name, desc[0], "zeldaRelicResources
         }
     }
 
+
+    object ProceedButtonPatch {
+        @JvmStatic
+        fun Instrument(): ExprEditor {
+            return object : ExprEditor() {
+                @Throws(CannotCompileException::class)
+                override fun edit(i: Instanceof) {
+                    try {
+                        if (i.type.name == Mushrooms::class.java.name) {
+                            i.replace("\$_ = \$proceed($$) || currentRoom.event instanceof zeldaRelics.events.TunicMonsterEvent;")
+                        }
+                    } catch (e: NotFoundException) {
+                        println(e)
+                    }
+                }
+            }
+        }
+    }
 
 }

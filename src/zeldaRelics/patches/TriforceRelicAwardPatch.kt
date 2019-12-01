bf9678750d8ef4c8bafe68a71c2c1b5cca828c8c
velvet-halation <buizark@gmail.com>
2019-12-01 00:15:10 -0800

    hee

--- src/zeldaRelics/patches/TriforceRelicAwardPatch.kt
+++ src/zeldaRelics/patches/TriforceRelicAwardPatch.kt
@@ -2,9 +2,12 @@ package zeldaRelics.patches
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
 import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.helpers.RelicLibrary
 import com.megacrit.cardcrawl.relics.AbstractRelic
 import com.megacrit.cardcrawl.rewards.RewardItem
+import com.megacrit.cardcrawl.rooms.MonsterRoomElite
+import com.megacrit.cardcrawl.rooms.TreasureRoom
 import zeldaRelics.helpers.Helper
 import zeldaRelics.modifiers.Hylian
 import zeldaRelics.relics.*
@@ -21,23 +24,25 @@ class TriforceRelicAwardPatch {
         @JvmStatic
         fun Postfix(__instance: RewardItem, relic: AbstractRelic) {
             if (CardCrawlGame.trial == null || !CardCrawlGame.trial.dailyModIDs().contains(Hylian.id)) return
-            if (!player.hasRelic(CourageTriforce.id)) {
-                val piece = RelicLibrary.getRelic(CouragePiece.id).makeCopy()
-                __instance.relic = piece
-                __instance.text =  piece.name
-                return
-            }
-            if (!player.hasRelic(WisdomTriforce.id)) {
-                val piece = RelicLibrary.getRelic(WisdomPiece.id).makeCopy()
-                __instance.relic = piece
-                __instance.text =  piece.name
-                return
-            }
-            if (!player.hasRelic(PowerTriforce.id)) {
-                val piece = RelicLibrary.getRelic(PowerPiece.id).makeCopy()
-                __instance.relic = piece
-                __instance.text =  piece.name
-                return
+            if (AbstractDungeon.getCurrRoom() is MonsterRoomElite || AbstractDungeon.getCurrRoom() is TreasureRoom) {
+                if (!player.hasRelic(CourageTriforce.id)) {
+                    val piece = RelicLibrary.getRelic(CouragePiece.id).makeCopy()
+                    __instance.relic = piece
+                    __instance.text =  piece.name
+                    return
+                }
+                if (!player.hasRelic(WisdomTriforce.id)) {
+                    val piece = RelicLibrary.getRelic(WisdomPiece.id).makeCopy()
+                    __instance.relic = piece
+                    __instance.text =  piece.name
+                    return
+                }
+                if (!player.hasRelic(PowerTriforce.id)) {
+                    val piece = RelicLibrary.getRelic(PowerPiece.id).makeCopy()
+                    __instance.relic = piece
+                    __instance.text =  piece.name
+                    return
+                }
             }
         }
     }

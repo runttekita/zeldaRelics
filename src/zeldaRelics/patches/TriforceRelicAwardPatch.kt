e25f8a154907e0979cd973aafd9ab6525436df97
velvet-halation <buizark@gmail.com>
2019-11-30 17:25:25 -0800

    wheee

--- src/zeldaRelics/patches/TriforceRelicAwardPatch.kt
+++ src/zeldaRelics/patches/TriforceRelicAwardPatch.kt
@@ -1,10 +1,35 @@
 package zeldaRelics.patches
 
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+import com.megacrit.cardcrawl.helpers.RelicLibrary
+import com.megacrit.cardcrawl.relics.AbstractRelic
+import com.megacrit.cardcrawl.rewards.RewardItem
+import zeldaRelics.helpers.Helper
+import zeldaRelics.relics.*
 
 @SpirePatch(
-
+    clz = RewardItem::class,
+    method = SpirePatch.CONSTRUCTOR,
+    paramtypez = [
+        AbstractRelic::class
+    ]
 )
 class TriforceRelicAwardPatch {
-    
+    companion object : Helper {
+        @JvmStatic
+        fun Postfix(__instance: RewardItem, relic: AbstractRelic) {
+            if (!player.hasRelic(CourageTriforce.id)) {
+                __instance.relic = RelicLibrary.getRelic(CouragePiece.id).makeCopy()
+                return
+            }
+            if (!player.hasRelic(WisdomTriforce.id)) {
+                __instance.relic = RelicLibrary.getRelic(WisdomPiece.id).makeCopy()
+                return
+            }
+            if (!player.hasRelic(PowerTriforce.id)) {
+                __instance.relic = RelicLibrary.getRelic(PowerPiece.id).makeCopy()
+                return
+            }
+        }
+    }
 }
